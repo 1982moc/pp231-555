7ed89a64b95cd4864c4f2bb2745eb7874e4d99f2
1982moc <1982moc@gmail.com>
2024-03-21 16:46:04 +0100

    111

--- src/main/java/web/dao/UserDao.java
+++ src/main/java/web/dao/UserDao.java
@@ -1,2 +1,13 @@
-package web.dao;public interface UserDao {
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User> getUsersList();
+    User getUser(int id);
+    void addUser(User user);
+    void deleteUser(int id);
+    void editUser(User user);
 }
--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -1,2 +1,13 @@
-package web.service;public interface UserService {
+package web.service;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getUsersList();
+    User getUser(int id);
+    void addUser(User user);
+    void deleteUser(int id);
+    void editUser(User user);
 }
