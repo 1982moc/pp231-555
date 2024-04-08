f474248f7953048bb60299fae2cccf7a97a32837
1982moc <1982moc@gmail.com>
2024-04-08 12:20:40 +0200

    1111

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,11 +1,19 @@
 package web.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import javax.persistence.*;
+
+
+
 @Entity
 @Table(name = "users")
 public class User {
